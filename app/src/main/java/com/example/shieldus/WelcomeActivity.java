761fb1dc273bd0e52af2d0f3998201439ccdf55b
Valentina Ferrentino <v.ferrentino10@studenti.unisa.it>
2025-07-25 16:45:15 +0200

    Added themes and icons

---
+++ app/src/main/java/com/example/shieldus/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.shieldus;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            finish();
+        }, SPLASH_DURATION);
+    }
+}
